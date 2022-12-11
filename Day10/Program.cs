a4d1173882bd6b98f3fb2810525cf86f10e1e0d2
Legolaszstudio <novotny.levente@gmail.com>
2022-12-11 21:38:28 +0100

    Day10 Part2

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -1,6 +1,7 @@
-﻿string[] file = File.ReadLines("G:\\003_PROGRAMOZAS\\aoc2022\\Day10\\bExample.txt").ToArray();
+﻿string[] file = File.ReadLines("G:\\003_PROGRAMOZAS\\aoc2022\\Day10\\input.txt").ToArray();
 int registerValue = 1;
 List<int> registerValues = new List<int>();
+registerValues.Add(registerValue);
 
 foreach (string line in file) {
     if (line.StartsWith("addx ")) {
@@ -9,18 +10,47 @@ foreach (string line in file) {
         registerValue += incrementer;
     }
     registerValues.Add(registerValue);
-    Console.WriteLine(registerValue);
 }
 
 List<int> neededVals = new List<int>();
-neededVals.Add(registerValues[20-2] * 20);
-neededVals.Add(registerValues[60-2] * 60);
-neededVals.Add(registerValues[100-2] * 100);
-neededVals.Add(registerValues[140-2] * 140);
-neededVals.Add(registerValues[180-2] * 180);
-neededVals.Add(registerValues[220-2] * 220);
+neededVals.Add(registerValues[20-1] * 20);
+neededVals.Add(registerValues[60-1] * 60);
+neededVals.Add(registerValues[100-1] * 100);
+neededVals.Add(registerValues[140-1] * 140);
+neededVals.Add(registerValues[180-1] * 180);
+neededVals.Add(registerValues[220-1] * 220);
 
 Console.WriteLine(neededVals.Sum());
 
 
 // ---------- Part Two ----------
+void printCrt(List<List<int>> crt) {
+    foreach (List<int> line in crt) {
+        foreach (int pixel in line) {
+            Console.Write(pixel == 0 ? " " : "#");
+        }
+        Console.WriteLine();
+    }
+}
+
+List<List<int>> crt = new List<List<int>>();
+for (int i = 0; i < 6; i++) {
+    crt.Add(new List<int>());
+    crt.Last().AddRange(Enumerable.Repeat(0, 40));
+}
+
+int crt_x = 0;
+int crt_y = 0;
+for (int cyc = 1; (cyc-1) < registerValues.Count; cyc++) {
+    int currValue = registerValues[cyc-1];
+    if (currValue == crt_x || currValue == crt_x - 1 || currValue == crt_x + 1) {
+        crt[crt_y][crt_x] = 1;
+    }
+    crt_x++;
+    if (cyc % 40 == 0) {
+        crt_y += 1;
+        crt_x = 0;
+    }
+}
+
+printCrt(crt);
